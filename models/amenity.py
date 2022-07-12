fba64e4b5c0b35ca342251f15da5f3a69959b0ed
Youssef Sahbani <youssefmasakni@gmail.com>
2022-07-12 02:10:18 -0700

    models

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Amenity Module"""
+from models.base_model import BaseModel
+import models
+
+
+class Amenity(BaseModel):
+    """Class for Amenity Objects
+    Attributes:
+    name: string - empty string
+    """
+    name = str()
